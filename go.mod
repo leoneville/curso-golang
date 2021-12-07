02ec4861c8ca1347cafa27104075108658fb7998
leoneville <leoneville_@hotmail.com>
2021-12-06 22:54:03 -0300

    Aprendendo sobre packages

---
+++ go.mod
@@ -0,0 +1,5 @@
+module curso
+
+go 1.17
+
+require github.com/leoneville/goarea v0.0.0-20211207012150-3e69132e65ba
---
+++ pacote/reuso/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/leoneville/goarea"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(5.0))
+	fmt.Println(goarea.Rect(5.0, 2.0))
+	// fmt.Println(area._TrianguloEq(5.0, 2.0))
+}
